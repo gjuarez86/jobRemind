e03a2ea0110a7ef39efcaf0cb97d411d9dc586f4
MacBook Pro <macbookpro@macbook-2012-23.local>
2016-06-07 15:25:37 -0700

    initial commit and basic server set up

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.send('hello');
+});
+
+app.listen(3000, function(){
+  console.log('Ready on Port 3000');
+});
+
