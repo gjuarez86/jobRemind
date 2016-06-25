6e16c9e611ce55356163d62c48900197b7bd5ab4
MacBook Pro <macbookpro@macbook-2012-23.local>
2016-06-25 13:06:40 -0700

    Added Twilio and notify for each new job posting.

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var app = express();
 var cheerio = require('cheerio');
 var request = require('request');
 var fs = require('fs');
+// Twilio Credentials 
+var accountSid = 'ACf2c336e0b36ace88e3b0308f0ebf0a59'; 
+var authToken = '2a38f364bc92c90dad926e1a279a3181'; 
+ 
+//require the Twilio module and create a REST client 
+var client = require('twilio')(accountSid, authToken); 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/jobRemind');
 
@@ -20,21 +26,30 @@ var recursive = function(req, res) {
 	request(url, function(error, response, html){
 		if(!error){
 			var $ = cheerio.load(html);
-			var title = null;
+			var newTitle = oldTitle;
 			var json = {title : ""};
 			$('#titletextonly').filter(function(){
 				var data = $(this);
-				title = data.text();
-				console.log(title);
-				json.title = title;
+				oldTitle = data.text();
+				// console.log(oldTitle);
+				json.title = oldTitle;
 			})
+			if((oldTitle !== newTitle)) {
+				client.sms.messages.create({ 
+				    to: "+13107353047", 
+				    from: "+15005550006",
+				    body: json.title
+				}, function(err, message) { 
+				    console.log('Sent from your twilio account -' + message.body); 
+				});
+			};
 
 			fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-				console.log('File successfully written! - Check your project directory for the output.json file');
+				// console.log('File successfully written! - Check your project directory for the output.json file');
 			});
 		}
 	})
-	setInterval(recursive, 2000);
+	setInterval(recursive, 60000);
 };
 app.get('/scrape', recursive);
 
