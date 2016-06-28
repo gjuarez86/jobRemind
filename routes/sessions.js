3493f85c6b50129b5443f0fd18cd1240eb29dbeb
MacBook Pro <macbookpro@macbook-2012-23.local>
2016-06-28 16:37:50 -0700

    Updated login and sign up forms. I can now login/signUp

--- routes/sessions.js
+++ routes/sessions.js
@@ -1,11 +1,27 @@
 var router = require('express').Router();
 var Users = require('../Models/user');
+var bcrypt = require('bcrypt');
+var React = require('react');
+var ReactDomServer = require('react-dom/server');
 
-
-router.post('/login', function(req, res){
-	Users.create(req.body);
-	console.log('got it');
+router.post('/signup', function(req, res){
+	var saltRounds = 10
+	var salt = bcrypt.genSaltSync(saltRounds);
+	var hash = bcrypt.hashSync(req.body.password, salt);
+	Users.create({username: req.body.username, password: hash});
+	res.redirect('/loggedin.html');
 });
 
+router.post('/login', function(req, res){
+	Users.findOne({username: req.body.username}, function(err, user){
+		if(bcrypt.compareSync(req.body.password, user.password)){
+			res.redirect('/loggedin.html')
+
+		} else {
+			console.log('Wrong password. Or Not Signed up')
+		};
+	})
+
+});
 
 module.exports = router;
--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,9 @@ module.exports = {
         {
           test: /\.jsx?$/,
           exclude: /node_modules/,
-          loader: "babel",
+          loader: 'babel',
           query: {
-              presets: ["react"]
+              presets: ['es2015', 'react']
             }
         }
       ]
